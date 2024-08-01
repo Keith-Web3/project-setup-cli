dab2b4522819b58493b89e6e1f309432eea05a24
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-08-01 14:30:55 +0100

    feat: added tags

--- src/commands.js
+++ src/commands.js
@@ -2,16 +2,30 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { setup } from './utils.js'
 
-console.log('commands')
-
 yargs(hideBin(process.argv))
   .command(
     'create <framework> <name>',
     'create new project',
-    () => {},
+    yargs => {
+      yargs.positional('framework', {
+        type: 'string',
+        describe: 'framework to setup',
+      })
+    },
     argv => {
-      setup(argv.name, argv.framework)
+      const tags = argv.tags.split(' ').reduce((acc, curr) => {
+        const newVal = curr.trim()
+        if ('  '.includes(newVal)) return acc
+        return [...acc, `--${newVal}`]
+      }, [])
+
+      setup(argv.name, argv.framework, tags)
     }
   )
+  .option('tags', {
+    alias: 't',
+    type: 'string',
+    description: 'just a random tag',
+  })
   .demandCommand(1)
   .parse()
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,26 @@
 import { spawn } from 'node:child_process'
 
+const NEXTJS_DEFAULT_TAGS = [
+  '--ts',
+  '--eslint',
+  '--app',
+  '--src-dir',
+  '--import-alias @/*',
+]
+
 export const setup = function (name, framework, tags) {
   const lang = framework.trim().toLowerCase()
+  if (tags.includes('--default')) {
+    const idx = tags.findIndex(el => el === '--default')
+
+    if (idx === -1) return
+    tags.splice(idx, 1, ...NEXTJS_DEFAULT_TAGS)
+  }
+  const tagsString = tags.join(' ')
 
   if (lang === 'nextjs') {
     const command = 'npx'
-    const args = ['create-next-app@latest', name, '--ts --eslint --app']
+    const args = ['create-next-app@latest', name, tagsString]
 
     console.log(new URL(`./${name}`, import.meta.url).pathname)
 
